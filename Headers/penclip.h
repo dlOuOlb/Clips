288b80f4257ddd9f8b03512bc1d909f1b14d1e48
Ranny Clover <36104212+dlOuOlb@users.noreply.github.com>
2018-02-21 08:38:34 +0900

    Update penclip.h

--- Headers/penclip.h
+++ Headers/penclip.h
@@ -2,7 +2,7 @@
 /*	PenClip is a file I/O header.									*/
 /*																	*/
 /*	Written by Ranny Clover								Date		*/
-/*	http://github.com/dlOuOlb/Clips						2018.02.12	*/
+/*	http://github.com/dlOuOlb/Clips/					2018.02.12	*/
 /*------------------------------------------------------------------*/
 /*	OpenCL Support													*/
 /*	http://www.khronos.org/opencl/									*/
