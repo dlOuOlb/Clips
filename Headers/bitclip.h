952946ead603071c76024a276a06a1f056fa8542
Ranny Clover <36104212+dlOuOlb@users.noreply.github.com>
2018-06-20 20:32:54 +0900

    Add files via upload

--- Headers/bitclip.h
+++ Headers/bitclip.h
@@ -2,7 +2,7 @@
 /*	BitClip specifies the size of data types.						*/
 /*																	*/
 /*	Written by Ranny Clover								Date		*/
-/*	http://github.com/dlOuOlb/Clips/					2018.06.19	*/
+/*	http://github.com/dlOuOlb/Clips/					2018.06.20	*/
 /*------------------------------------------------------------------*/
 /*	OpenCL Support													*/
 /*	http://www.khronos.org/opencl/									*/
