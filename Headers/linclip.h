91e5d21357d18689fa254c2bb21c6e5fecf91588
Ranny Clover <36104212+dlOuOlb@users.noreply.github.com>
2019-03-13 17:22:12 +0900

    Add files via upload

--- Headers/linclip.h
+++ Headers/linclip.h
@@ -2,7 +2,7 @@
 /*	LinClip provides some elementary arithmetic operations.			*/
 /*																	*/
 /*	Written by Ranny Clover								Date		*/
-/*	http://github.com/dlOuOlb/Clips/					2019.03.11	*/
+/*	http://github.com/dlOuOlb/Clips/					2019.03.13	*/
 /*------------------------------------------------------------------*/
 /*	OpenCL Support													*/
 /*	http://www.khronos.org/opencl/									*/
