58d4f7782c441b6663558047caff35c5a2446a12
Ranny Clover <36104212+dlOuOlb@users.noreply.github.com>
2019-06-03 15:30:45 +0900

    Add files via upload

--- Headers/timclip.h
+++ Headers/timclip.h
@@ -2,13 +2,14 @@
 /*	TimClip is a simple time record library.						*/
 /*																	*/
 /*	Written by Ranny Clover								Date		*/
-/*	http://github.com/dlOuOlb/Clips/					2019.05.27	*/
+/*	http://github.com/dlOuOlb/Clips/					2019.06.03	*/
 /*------------------------------------------------------------------*/
 
 #ifndef _INC_TIMCLIP
 #define _INC_TIMCLIP
 
 #if(1)
+#include <math.h>
 #include <time.h>
 #include <limits.h>
 #include <bitclip.h>
@@ -31,6 +32,14 @@ struct _timc_sw
 	ADDRESS Nums;		//TimClip : Number of Timers
 };
 MemC_Type_Declare_(struct,timc_sw,TIMC_SW);	//TimClip : Stopwatch Structure
+
+//TimClip : Random Generator Structure
+struct _timc_rg
+{
+	data_64 _PL_ State;	//TimClip : Internal State Set
+	ADDRESS Nums;		//TimClip : Number of States
+};
+MemC_Type_Declare_(struct,timc_rg,TIMC_RG);	//TimClip : Random Generator Structure
 #endif
 
 #if(Fold_(Library Casing))
@@ -39,6 +48,14 @@ extern const struct _timcase
 {
 	//TimClip : Library Version
 	BYTE_08 _PL_ Version;
+
+	//TimClip : Timer Clock's Resolution and Frequency
+	const struct
+	{
+		REAL_32 Resolution;	//TimClip : clocks per second
+		REAL_32 Frequency;	//TimClip : seconds per clock
+	}
+	Clock;
 
 	//TimClip : Stopwatch Functions
 	const struct
@@ -91,13 +108,68 @@ extern const struct _timcase
 	}
 	SW;
 
-	//TimClip : Timer Clock's Resolution and Frequency
+	//TimClip : Random Generator Functions
 	const struct
 	{
-		REAL_32 Resolution;	//TimClip : clocks per second
-		REAL_32 Frequency;	//TimClip : seconds per clock
+		//TimClip : Random Generator Memory Allocation - Deallocate with "TimC.RG.Delete_"
+		timc_rg*(_PL_ Create_)(ADDRESS StatesNumber);
+		//TimClip : Random Generator Memory Deallocation
+		general(_PL_ Delete_)(timc_rg *_PL_ RandomGenerator);
+
+		//TimClip : Random Generator Memory Occupation
+		address(_PL_ Size_)(TIMC_RG _PL_ RandomGenerator);
+
+		//TimClip : Random Generator State Reset
+		//£ªRandomGenerator -> State[idx]£½time(NULL)£«idx;
+		//£ªReturn value is ~0 for success, 0 for failure.
+		boolean(_PL_ Reset_)(TIMC_RG _PL_ RandomGenerator);
+
+		//TimClip : Random Generation Functions
+		const struct
+		{
+			//TimClip : Random Generation for Specified Bytes
+			//£ªMWC64X algorithm is used.
+			//£ªReturn value is ~0 for success, 0 for failure.
+			boolean(_PL_ Byte_)(TIMC_RG _PL_ RandomGenerator,ADDRESS StateSelect,general _PL_ _R_ ArrayData,ADDRESS ArraySize);
+			//TimClip : Random Permutation
+			//£ªKnuth shuffles algorithm is used.
+			//£ªAn array whose length exceeds ((2^64)-1) is not allowed.
+			//£ªReturn value is ~0 for success, 0 for failure.
+			boolean(_PL_ Perm_)(TIMC_RG _PL_ RandomGenerator,ADDRESS StateSelect,address _PL_ _R_ Array,ADDRESS Length);
+		}
+		Rand;
+
+		//TimClip : Random Uniform Functions
+		const struct
+		{
+			//TimClip : 32-bit Integer Array of Uniform Distribution U[L, H]
+			//£ªReturn value is ~0 for success, 0 for failure.
+			boolean(_PL_ I32_)(TIMC_RG _PL_ RandomGenerator,ADDRESS StateSelect,inte_32 _PL_ _R_ Array,ADDRESS Length,INTE_32 Low,INTE_32 High);
+
+			//TimClip : 32-bit Real Array of Uniform Distribution U[L, H)
+			//£ªReturn value is ~0 for success, 0 for failure.
+			boolean(_PL_ R32_)(TIMC_RG _PL_ RandomGenerator,ADDRESS StateSelect,real_32 _PL_ _R_ Array,ADDRESS Length,REAL_32 Low,REAL_32 High);
+			//TimClip : 64-bit Real Array of Uniform Distribution U[L, H)
+			//£ªReturn value is ~0 for success, 0 for failure.
+			boolean(_PL_ R64_)(TIMC_RG _PL_ RandomGenerator,ADDRESS StateSelect,real_64 _PL_ _R_ Array,ADDRESS Length,REAL_64 Low,REAL_64 High);
+		}
+		Uni;
+
+		//TimClip : Random Gaussian Functions
+		const struct
+		{
+			//TimClip : 32-bit Real Array of Gaussian Distribution N(¥ì, ¥ò©÷)
+			//£ªBox-Muller transform is used.
+			//£ªReturn value is ~0 for success, 0 for failure.
+			boolean(_PL_ R32_)(TIMC_RG _PL_ RandomGenerator,ADDRESS StateSelect,real_32 _PL_ _R_ Array,ADDRESS Length,REAL_32 Mean,REAL_32 Deviation);
+			//TimClip : 64-bit Real Array of Gaussian Distribution N(¥ì, ¥ò©÷)
+			//£ªBox-Muller transform is used.
+			//£ªReturn value is ~0 for success, 0 for failure.
+			boolean(_PL_ R64_)(TIMC_RG _PL_ RandomGenerator,ADDRESS StateSelect,real_64 _PL_ _R_ Array,ADDRESS Length,REAL_64 Mean,REAL_64 Deviation);
+		}
+		Gau;
 	}
-	Clock;
+	RG;
 }
 TimC;
 #endif
